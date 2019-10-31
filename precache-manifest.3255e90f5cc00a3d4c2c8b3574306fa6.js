b5159fcc0e039f87e1ef0875bd944c2330b013b6
MeowKawaii <nanthikan.ae@gmail.com>
2019-10-31 22:00:21 +0700

    Updates

---
+++ precache-manifest.3255e90f5cc00a3d4c2c8b3574306fa6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b94fcdd38cd184f862e56cbdded7afb3",
+    "url": "/ywc/index.html"
+  },
+  {
+    "revision": "089c6efba50eb5610d17",
+    "url": "/ywc/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "a4c267aeb15c78c12ef2",
+    "url": "/ywc/static/js/2.7c6ce6b4.chunk.js"
+  },
+  {
+    "revision": "089c6efba50eb5610d17",
+    "url": "/ywc/static/js/main.b63a94e5.chunk.js"
+  },
+  {
+    "revision": "7e37431b456f38d800f9",
+    "url": "/ywc/static/js/runtime-main.a48c6c76.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/ywc/static/media/logo.25bf045c.svg"
+  }
+]);
