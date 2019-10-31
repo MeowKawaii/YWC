b489daa7eddaa48ea4295384f31ce46edcb1f23e
MeowKawaii <nanthikan.ae@gmail.com>
2019-11-01 02:27:14 +0700

    Add Picture in Banner

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import React, { Fragment } from "react";
 import "./App.css";
-import { Container, Row, Col } from "react-bootstrap";
 import MyNavbar from "./components/MyNavbar";
+import MyBanner from "./components/MyBanner";
 
 function App() {
   return (
     <Fragment>
       <MyNavbar />
-      <div style={{ width: "100%", backgroundColor: "red",zIndex:'-1' }}>eiei</div>
-      <div style={{ height: "1000px" }}>ทดสอบๆ</div>
+      <MyBanner />
     </Fragment>
   );
 }
---
+++ src/components/MyBanner/index.jsx
@@ -0,0 +1,47 @@
+import React, { Component, Fragment } from "react";
+import { Row, Col } from "react-bootstrap";
+
+export class MyBanner extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <Fragment>
+        <Row>
+          <Col xs={4} md={4} style={{ padding: 0 }}>
+            <img
+              src="https://www.xn--b3caa1e2a7e2b0h2be.com/img/hero_banner/banner-left.png"
+              alt="banner-left"
+              style={{ width: "100%", height: "80%" }}
+            />
+          </Col>
+          <Col xs={4} md={4} style={{ padding: 0, position: "relative" }}>
+            <img
+              src="https://www.xn--b3caa1e2a7e2b0h2be.com/img/logo/banner.png"
+              alt="banner"
+              style={{ position: "absolute",width:"50%",marginTop:'10%',marginLeft:'25%' }}
+            />
+            <img
+              src="https://www.xn--b3caa1e2a7e2b0h2be.com/img/hero_banner/banner-center.png"
+              alt="banner-center"
+              style={{ width: "100%", height: "80%" }}
+            />
+          </Col>
+          <Col xs={4} md={4} style={{ padding: 0 }}>
+            <img
+              src="https://www.xn--b3caa1e2a7e2b0h2be.com/img/hero_banner/banner-right.png"
+              alt="banner-right"
+              style={{ width: "100%", height: "80%" }}
+            />
+          </Col>
+        </Row>
+      </Fragment>
+    );
+  }
+}
+
+export default MyBanner;
