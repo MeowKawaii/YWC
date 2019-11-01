cda863111798a40eadd5980fdc0ce2e93e253421
MeowKawaii <nanthikan.ae@gmail.com>
2019-11-02 01:12:16 +0700

    Fetch data

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from "react";
+import React, { Component, Fragment } from "react";
 import "./App.css";
 import MyNavbar from "./components/MyNavbar";
 import MyBanner from "./components/MyBanner";
@@ -7,19 +7,51 @@ import Detail from "./components/Detail";
 import BoxTAT from "./components/BoxTAT";
 import BoxKTC from "./components/BoxKTC";
 import Footer from "./components/Footer";
+import { Button } from "react-bootstrap";
 
-function App() {
-  return (
-    <Fragment>
-      <MyNavbar />
-      <MyBanner />
-      <RegisterBox />
-      <Detail />
-      <BoxTAT />
-      <BoxKTC />
-      <Footer />
-    </Fragment>
-  );
+export class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      data: {}
+    };
+  }
+
+  componentDidMount() {
+    this.getData();
+  }
+
+  render() {
+    return (
+      <Fragment>
+        <MyNavbar />
+        <MyBanner />
+        <RegisterBox />
+        <Button
+          onClick={() => {
+            console.log(this.state.data);
+          }}
+        >
+          data
+        </Button>
+        <Detail />
+        <BoxTAT />
+        <BoxKTC />
+        <Footer />
+      </Fragment>
+    );
+  }
+
+  getData = () => {
+    fetch(`https://panjs.com/ywc.json`, {
+      method: "GET"
+    })
+      .then(response => response.json())
+      .then(data => {
+        this.setState({ data: data });
+      });
+  };
 }
 
 export default App;
